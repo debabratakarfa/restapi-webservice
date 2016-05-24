7615da8f2f4e9df0b0f04463bb3ea1bc7926f107
Debabrata Karfa <adkarfa@gmail.com>
2016-05-25 01:44:13 +0530

    Update
    Update on config

--- dbconfig.php
+++ dbconfig.php
@@ -1,15 +1,15 @@
 <?php
- 
-function connect_db() {
-    try {
-	    $db_user = 'debim_ws';
-	    $db_pass = 'M3D$C?(ds%#Hl72hI.';
-        $connection = new PDO('mysql:host=localhost;dbname=debim_webservice', $db_user, $db_pass);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    } catch(PDOException $e) {
-        echo 'ERROR: ' . $e->getMessage();
-    }
+function connect_db() {
+
+    	$connection = new mysqli(
+    	    getenv('OPENSHIFT_MYSQL_DB_HOST'),
+    	    getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+    	    getenv('OPENSHIFT_MYSQL_DB_HOST'),
+    	    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    	    getenv('OPENSHIFT_MYSQL_DB_PORT')
+    	);
+
     return $connection;
 }
-
+?>
--- index.php
+++ index.php
@@ -1,52 +1,47 @@
-<?php
+  <?php
 
-require 'vendor/autoload.php';
-require 'dbconfig.php';
- 
-$app = new Slim\App();
+  require 'vendor/autoload.php';
+  require 'dbconfig.php';
 
-//slim application routes
-$app->get('/', function ($request, $response, $args) { 
- $response->write("Welcome to RESTful API Webservices");
- return $response;
-});
- 
-$app->get('/demo', function ($request, $response, $args) {
- $response->write("Hello Demo Content!");
- return $response;
-});
+  $app = new Slim\App();
 
-$app->get('/users', 'getUsers');
+  //slim application routes
+  $app->get('/', function ($request, $response, $args) {
+   $response->write("Welcome to RESTful API Webservices");
+   return $response;
+  });
 
-$app->get('/users/{id}', function ($request, $response, $args) {
-	getUser($args['id']);
-});
+  $app->get('/demo', function ($request, $response, $args) {
+   $response->write("Hello Demo Content!");
+   return $response;
+  });
 
-$app->post('/users', 'addUser');
-$app->put('/users/:id', 'updateUser');
-$app->delete('/users/:id', 'deleteUser');
-$app->run();
+  $app->get('/users', 'getUsers');
 
-function getUsers() {
-    $sql_query = "select * FROM users ORDER BY user_name";
-    try {
-        $dbCon = getConnection();
-        $stmt   = $dbCon->query($sql_query);
-        $users  = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $dbCon = null;
-        echo '{"users": ' . json_encode($users) . '}';
-    }
-    catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }   
-}
+  $app->get('/users/{id}', function ($request, $response, $args) {
+  	getUser($args['id']);
+  });
 
-function getUser($id) {
-	$sql_query = "select * FROM users WHERE users_id=$id";
-    $dbCon = connect_db();
-    $stmt = $dbCon->query($sql_query);
-    $user = $stmt->fetch_all(MYSQLI_ASSOC);
-    $dbCon = null;
-    echo '{"user": ' . json_encode($user) . '}';
+  $app->post('/users', 'addUser');
+  $app->put('/users/:id', 'updateUser');
+  $app->delete('/users/:id', 'deleteUser');
+  $app->run();
 
-}
+  function getUsers() {
+  	$sql_query = "select * FROM users ORDER BY user_name";
+      $dbCon = connect_db();
+      $sql_exe = $dbCon->query($sql_query);
+      $users = $sql_exe->fetch_all(MYSQLI_ASSOC);
+      $dbCon = null;
+      echo '{"users": ' . json_encode($users) . '}';
+  }
+
+  function getUser($id) {
+  	$sql_query = "select * FROM users WHERE users_id=$id";
+      $dbCon = connect_db();
+      $stmt = $dbCon->query($sql_query);
+      $user = $stmt->fetch_all(MYSQLI_ASSOC);
+      $dbCon = null;
+      echo '{"user": ' . json_encode($user) . '}';
+  }
+?>
